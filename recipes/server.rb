73902574739284e1356b0e356283723977f7d96a
Elijah Wright <elijah.wright@gmail.com>
2016-02-19 15:52:17 -0600

    Update server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,14 +7,14 @@ package "Tableau Server #{node.tableau_server["version"]} (#{node.tableau_server
   options "/verysilent"
 end
 
-databag = Chef::EncryptedDataBagItem.load("tableau_server", node.chef_environment)
+# databag = Chef::EncryptedDataBagItem.load("tableau_server", node.chef_environment)
 
 # It is a pain that we have to run this every chef run but I can't find a
 # way to check if we have already registered.
-execute "Register Tableau Server" do
-  command "tabinstallck.exe -activate #{databag["product_key"]}"
-  cwd 'C:\Program Files\Tableau\Tableau Server\\' + node.tableau_server["version"] + '\bin'
-end
+#execute "Register Tableau Server" do
+#  command "tabinstallck.exe -activate #{databag["product_key"]}"
+#  cwd 'C:\Program Files\Tableau\Tableau Server\\' + node.tableau_server["version"] + '\bin'
+#end
 
 windows_firewall_rule "Tableau Server" do
   protocol "TCP"
